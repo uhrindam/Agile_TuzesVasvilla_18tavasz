51a06aa1f7433e1becd73ff47dc732efd9ef5b75
gergo sipos <prosipinho@gmail.com>
2018-02-24 14:09:57 +0100

    initial commit, .net core project with swagger docs

---
+++ AgileTuzesVasvilla/AgileTuzesVasvilla/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgileTuzesVasvilla.Controllers
+{
+    [Route("[Controller]/")]
+    public class HomeController : Controller
+    {
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return new JsonResult(new int[] { 42, 69, 125, 444 });
+        }
+    }
+}
